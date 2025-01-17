f18ff9238329b5b52245aa9c246bd80d176362d9
adaiacamacho <adaianicol.camachova@elorrieta-errekamari.com>
2025-01-17 09:36:55 +0100

    Desconexión solucionada

--- Reto2Grupo2/src/Controlador/HiloConexion.java
+++ Reto2Grupo2/src/Controlador/HiloConexion.java
@@ -7,6 +7,7 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.List;
 
 import Modelo.Users;
@@ -14,6 +15,7 @@ import Modelo.Users;
 public class HiloConexion extends Thread {
 
 	Socket clienteSocket;
+	 boolean continuar = true;
 
 	public HiloConexion(Socket clienteSocket) {
 		this.clienteSocket = clienteSocket;
@@ -21,52 +23,59 @@ public class HiloConexion extends Thread {
 
 	@Override
 	public void run() {
-	    boolean continuar = true;
-
 	    try (
-	    		ObjectOutputStream oos = new ObjectOutputStream(clienteSocket.getOutputStream());
-	    		ObjectInputStream ois = new ObjectInputStream(clienteSocket.getInputStream());
-	    		DataOutputStream dos = new DataOutputStream(clienteSocket.getOutputStream());
-	    		DataInputStream dis = new DataInputStream(clienteSocket.getInputStream())) {
-
+	        ObjectOutputStream oos = new ObjectOutputStream(clienteSocket.getOutputStream());
+	        ObjectInputStream ois = new ObjectInputStream(clienteSocket.getInputStream());
+	        DataOutputStream dos = new DataOutputStream(clienteSocket.getOutputStream());
+	        DataInputStream dis = new DataInputStream(clienteSocket.getInputStream())
+	    ) {
 	        while (continuar) {
 	            try {
-	                int accion = (int) dis.readInt();
+	                int accion = dis.readInt(); // Leer acción enviada por el cliente
 	                switch (accion) {
-	                case 1:
-	                    login(dis, dos);
-	                    break;
-	                case 2:
-	                    mostrarHorario(dis, dos);
-	                    break;
-	                case 3:
-	                    mostrarOtrosHorarios(dis, dos);
-	                    break;
-	                default:
-	                    continuar = false;
-	                    break;
+	                    case 1:
+	                        login(dis, dos);
+	                        break;
+	                       
+	                    case -1:
+	                        System.out.println("Cliente solicitó desconexión.");
+	                        continuar = false;
+	                        break;
+	                    default:
+	                        System.out.println("Acción no reconocida: " + accion);
+	                        break;
 	                }
-	            } catch (EOFException e) {
-	                System.out.println("EOF alcanzado. Cerrando la conexión...");
+	            } catch (EOFException | SocketException e) {
+	                System.out.println("Cliente desconectado.");
 	                continuar = false;
 	            }
 	        }
 	    } catch (IOException e) {
 	        e.printStackTrace();
+	    } finally {
+	        try {
+	            if (clienteSocket != null && !clienteSocket.isClosed()) {
+	                clienteSocket.close();
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
 	    }
 	}
 
+
+
 	private void login(DataInputStream dis, DataOutputStream dos) {
-		try {
-			String nombreUser = (String) dis.readUTF();
-			String pass = (String) dis.readUTF();
-			Users user = new Users().mObtenerUsuario(nombreUser, pass);
-			dos.writeUTF(user.getNombre());
-			dos.writeUTF(user.getPassword());
-			dos.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	    try {
+	        String nombreUser = dis.readUTF();
+	        String pass = dis.readUTF();
+	        Users user = new Users().mObtenerUsuario(nombreUser, pass);
+	        dos.writeUTF(user.getNombre());
+	        dos.writeUTF(user.getPassword());
+	        dos.flush();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
 	}
 
 	private void mostrarHorario(DataInputStream ois, DataOutputStream oos) {
@@ -91,5 +100,8 @@ public class HiloConexion extends Thread {
 		}*/
 
 	}
+	
+	
+
 
 }
